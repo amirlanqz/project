fbf9f6fb9a37810114e0201e4cd7cecc405d770c
Amirlan <amirlanqz@gmail.com>
2023-04-27 21:31:18 +0600

    changed app.js file and added engine.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 'use strict';
-let a = 'sss';
+const numberOfFilms = +prompt("How many film do you movie");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const oneFilmFromOfTheList = prompt("One film which do you watched");
+const priceOfFilm = prompt("price of film");
+
+personalMovieDB.movies[oneFilmFromOfTheList] = priceOfFilm;
+console.log(personalMovieDB);
