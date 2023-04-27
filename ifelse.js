7a0bf9addf0687908c41d9c80c77c41fb1ea8871
Amirlan <amirlanqz@gmail.com>
2023-04-28 00:49:33 +0600

    learn if else statement

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+'use strict';
+const myAge = +prompt('How old are you?');
+if (myAge >= 18) {
+    console.log('welcome to the club');
+} else {
+    console.log('you are young, come here when you will be old man');
+}
