b662a1067b3cd211e56e3cf3d4ccd658916ee4ed
Amirlan <amirlanqz@gmail.com>
2023-04-28 00:52:48 +0600

    added ternar operator in this file

--- ifelse.js
+++ ifelse.js
@@ -3,5 +3,7 @@ const myAge = +prompt('How old are you?');
 if (myAge >= 18) {
     console.log('welcome to the club');
 } else {
-    console.log('you are young, come here when you will be old man');
+    console.log('you are young, come when you will be old man');
 }
+
+(myAge == 24) ? console.log('its amirlan') : console.log('its not amirlan');;
