0e75af22673df05af98db6adf96e0e2d1a64f55a
Amirlan <amirlanqz@gmail.com>
2023-04-28 00:57:36 +0600

    added switch case operator in this file

--- ifelse.js
+++ ifelse.js
@@ -6,4 +6,19 @@ if (myAge >= 18) {
     console.log('you are young, come when you will be old man');
 }
 
-(myAge == 24) ? console.log('its amirlan') : console.log('its not amirlan');;
+(myAge == 24) ? console.log('its amirlan') : console.log('its not amirlan');
+
+switch (myAge) {
+    case 24:
+        console.log('amirlan');
+        break;
+    case 23:
+        console.log('aset');
+        break;
+    case 29:
+        console.log('marzhan');
+        break;
+    default:
+        console.log('error');
+        break;
+}
